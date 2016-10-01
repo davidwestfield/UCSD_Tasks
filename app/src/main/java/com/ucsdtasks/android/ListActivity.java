4c7f6eb6b24d8035631fa1c143cb2f857f677889
Daniel Sonnier <sonnier.daniel@gmail.com>
2016-10-01 16:27:33 -0700

    ListActivity finds user's location, prepared to request tasks

--- app/src/main/java/com/ucsdtasks/android/ListActivity.java
+++ app/src/main/java/com/ucsdtasks/android/ListActivity.java
@@ -1,11 +1,22 @@
 package com.ucsdtasks.android;
 
+import android.*;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
 
 
 /**
@@ -15,23 +26,59 @@ import android.widget.ListView;
  */
 
 public class ListActivity extends AppCompatActivity {
-    ListView listView ;
+    private ListView listView;
+    private LatLng currentLoc;
+    // private List<Task> tasks = new ArrayList<Task>();
+
+    private double SEARCH_RADIUS = 0.6; // TODO temporary
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+        // Checks if user has location enabled
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+            populateListView();
+        }
+        // User does not have location enabled, requests permissions
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+    }
+
+
+    /**
+     * Method populateListView
+     *
+     * This callback is triggered when the user approves locations permissions.
+     * It will enable location tracking and zoom to user's location.
+     */
+
+    public void populateListView() {
         // Get ListView object from xml
         listView = (ListView) findViewById(R.id.list);
 
+        // Requests tasks passing in user's current location and radius to look for tasks
+        //tasks = getTasks(currentLoc, SEARCH_RADIUS);
+
+        // Task objects will have ID, title, author, bid, description, and location
+
         // Values to be shown in ListView
         String[] values = new String[] {
-                "Sample 1",
-                "Sample 2",
-                "Sample 3",
-                "Sample 4",
-                "Sample 5"
+                "Walk dog",
+                "Buy milk",
+                "Sing a song",
+                "Bring me food",
+                "Etc"
         };
 
         // (Context, Layout for row, ID of TextView to write data to, Array of data)
@@ -58,5 +105,17 @@ public class ListActivity extends AppCompatActivity {
                 String itemName = (String) listView.getItemAtPosition(position);
             }
         });
+    }
+
+
+    /**
+     * Method onRequestPermissionsResults
+     *
+     * This callback is triggered when the user approves locations permissions.
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        populateListView();
     }
 }
