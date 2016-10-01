16d15b9c761e45043b3ade5131eb91a2f3319f78
Md Anil Shankar Jethani <ajethani11@gmail.com>
2016-10-01 16:03:39 -0700

    Added error checking and notify users when a task is submitted

--- app/src/main/java/com/ucsdtasks/android/CreateTask.java
+++ app/src/main/java/com/ucsdtasks/android/CreateTask.java
@@ -1,5 +1,7 @@
 package com.ucsdtasks.android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreateTask extends AppCompatActivity {
 
@@ -18,17 +21,6 @@ public class CreateTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_task);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     public void pushTask (View view) {
@@ -38,15 +30,20 @@ public class CreateTask extends AppCompatActivity {
         final EditText starting_offer_ET = (EditText) findViewById(R.id.starting_offer);
         final EditText location_ET = (EditText) findViewById(R.id.specify_location);
 
+        // Error checking
+        if(task_name_ET.getText().toString().matches("") ||
+                starting_offer_ET.getText().toString().matches("")) {
+            Snackbar.make(view, "Field(s) cannot be empty", Snackbar.LENGTH_SHORT).show();
+            return;
+        }
+
         // Storing and parsing from string to wanted data type
         task_name = task_name_ET.getText().toString();
-        location = starting_offer_ET.getText().toString();
-        starting_offer = Double.parseDouble(location_ET.getText().toString());
+        location = location_ET.getText().toString();
+        starting_offer = Double.parseDouble(starting_offer_ET.getText().toString());
+
+        Toast.makeText(this, "Task submitted", Toast.LENGTH_SHORT).show();
+
+        super.onBackPressed();
     }
-
-    public void doneEnteringData (View view) {
-
-        Snackbar.make(view, "Data stored", Snackbar.LENGTH_SHORT).show();
-    }
-
 }
