1d90d204eddce3089dfc1558b17c10c0988e4e2f
Md Anil Shankar Jethani <ajethani11@gmail.com>
2016-10-01 00:33:46 -0700

    Add map features

--- app/src/main/java/com/ucsdtasks/android/MapsActivity.java
+++ app/src/main/java/com/ucsdtasks/android/MapsActivity.java
@@ -1,5 +1,13 @@
 package com.ucsdtasks.android;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -16,10 +24,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public double latitudeM, longitudeM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         /*
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
@@ -31,11 +39,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     100);
         }
 */
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
     }
 
 
@@ -51,8 +63,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // This is price center
+        /* This is price center
         LatLng UCSD = new LatLng(32.879459, -117.237167);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UCSD, 15.0f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UCSD, 15.0f)); */
+
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);  // adding the +/- button on the map
+        mMap.getUiSettings().setAllGesturesEnabled(true);   // pinch to zoom on map
+        mMap.setMyLocationEnabled(true);    // Shows where we are currently
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String locationProvider = LocationManager.GPS_PROVIDER;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+
+        // Map On Click listener
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Create the marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                // Set marker position
+                markerOptions.position(latLng);
+
+                latitudeM = latLng.latitude;
+                longitudeM = latLng.longitude;
+
+                // Clear previous marker
+                mMap.clear();
+                // Animate it when a location is pressed
+                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                // Finally place the marker on the touched position
+                mMap.addMarker(markerOptions);
+            }
+        });
     }
+    // Needed to notify the user when a friend is nearby the marked position
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
 }
